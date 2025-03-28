cff7c1f01f0e36afb7f27b8070d0b656b8ecad06
Rafael de Paula Paiva <paiva.rafael@outlook.com>
2025-03-28 07:52:15 -0400

    Frontend improvement

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,31 @@ function clearLoginData() {
     }
 }
 
-const apiURL = '/api';
+const url_loc = window.location.pathname.split("/");
+console.log(url_loc);
+const last_path = url_loc[url_loc.length - 1];
+if (last_path === "" || last_path.endsWith(".html"))
+    url_loc.pop()
+url_loc.pop()
+url_loc.push("api")
+const apiURL = url_loc.join("/");
+console.log(apiURL);
+
+const FEED_DATA_TYPE = {
+    ip: "IP",
+    url: "URL",
+    domain: "Domain"
+}
+
+const FEED_ACT_NAME = {
+    create: "Adicionar",
+    edit: "Editar"
+}
+
+const ENTRY_ACT_NAME = {
+    create: "Nova Entrada",
+    edit: "Editar Entrada"
+}
 
 function app() {
     return {
@@ -39,10 +63,14 @@ function app() {
         selectedFeed: null,
         newEntry: clearEntry(),
 
+        feedFormCreate: true,
+        entryFormCreate: true,
+
         init() {
             axios.get(`${apiURL}/whoami`).then(response => {
                 //console.log(response)
                 this.user = response.data;
+                this.fetchFeeds().then();
             }).catch(() => {})
         },
 
@@ -70,6 +98,14 @@ function app() {
             }
         },
 
+        feedActionName() {
+            return this.feedFormCreate ? 'Adicionar' : 'Editar';
+        },
+
+        entryActionName() {
+            return this.feedFormCreate ? 'Adicionar' : 'Editar';
+        },
+
         async fetchFeeds() {
             try {
                 const response = await axios.get(`${apiURL}/feed`)
@@ -79,31 +115,71 @@ function app() {
             }
         },
 
+        async submitFeed() {
+            await (this.feedFormCreate ? this.createFeed() : this.editFeed());
+            this.resetFeed()
+        },
+
+        resetFeed() {
+            this.newFeed = clearFeed(),
+            this.feedFormCreate = true;
+        },
+
+        feedMarkToEdit(feed) {
+            this.newFeed = feed;
+            this.feedFormCreate = false;
+        },
+
+        async submitEntry() {
+            await (this.entryFormCreate ? this.createEntry() : this.editEntry());
+            this.resetEntry()
+        },
+
+        resetEntry() {
+            this.newEntry = clearEntry(),
+            this.entryFormCreate = true;
+        },
+
+        entryMarkToEdit(entry) {
+            this.newEntry = entry;
+            this.entryFormCreate = false;
+        },
+
+        unselectFeed() {
+            this.selectedFeed = null;
+            this.entries = [];
+        },
+
         // Category CRUD operations
         async createFeed() {
             if (this.newFeed.name.trim() === '') return;
             try {
                 const response = await axios.put(`${apiURL}/feed`, this.newFeed)
                 this.feeds.push(response.data.feed);
-                this.newFeed = clearFeed()
             } catch (error) {
                 console.error(error);
             }
         },
 
-        async removeFeed(id) {
+        async editFeed() {
+            try {
+                const response = await axios.put(`${apiURL}/feed`, this.newFeed)
+                const foundFeed = this.feeds.find(f => f.id === response.data.id)
+                foundFeed.description = this.newFeed.description;
+                foundFeed.is_public = this.newFeed.is_public;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+
+        async removeFeed(feed) {
             if (!confirm('Are you sure you want to delete this feed?'))
                 return;
             try {
-                const response = await axios.delete(`${apiURL}/feed/${id}`)
-                to_remove = this.feeds.findIndex(feed => feed.id !== response.data.id);
+                const response = await axios.delete(`${apiURL}/feed/${feed.id}`)
+                to_remove = this.feeds.findIndex(f => f.id !== response.data.id);
                 if (to_remove != -1)
                     this.feeds.splice(to_remove, 1);
-                if (this.selectedFeed.id === response.data.id) {
-                    this.entries = [];
-                    this.newEntry = clearEntry();
-                    this.selectedFeed = null;
-                }
             } catch (error) {
                 console.error(error);
             }
@@ -127,21 +203,19 @@ function app() {
             try {
                 const response = axios.put(`${apiURL}/entry/${feed.type}/${feed.id}`, this.newEntry)
                 this.entries.push(response.data.entry);
-                this.newEntry = clearEntry();
             } catch (error) {
                 console.error(error);
             }
         },
 
-        async editEntry(entry) {
-            const newName = prompt('Edit Item Name:', entry.name);
+        async editEntry() {
             const feed = this.selectedFeed;
-            if (newName === null || newName === entry.name) {
-                return;
-            }
             try {
-                const response = post(`${apiURL}/entry/${feed.type}/${feed.id}/${entry.id}`, { name: newName })
-                element = this.entries.find(e => e.id == response.data.id)
+                const response = post(`${apiURL}/entry/${feed.type}/${feed.id}/${entry.id}`, this.newEntry)
+                const foundEntry = this.entries.find(e => e.id == response.data.id)
+                foundEntry.enabled = this.newEntry.enabled;
+                foundEntry.description = this.newEntry.description;
+                foundEntry.valid_until = this.newEntry.valid_until;
             } catch (error) {
                 console.error(error);
             }
